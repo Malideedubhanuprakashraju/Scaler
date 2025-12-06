1703b5e8ac036e8d1d18538408c3fe6067178825
Malideedubhanuprakashraju <bhanuprakashmalideedu@gmail.com>
2025-12-06 13:32:47 +0530

    Beginner: Introduction to Beginner Module

--- IntoductionToPRogramming_1/src/Basics/Client.java
+++ IntoductionToPRogramming_1/src/Basics/Client.java
@@ -6,6 +6,24 @@ public class Client
 	public static void main(String[] args) 
 	{
 		 System.out.println("Bhanu");
+		 // Syntax 1: Statements should end with a semicolon ;
+		 // Syntax 2: Java is case sensitive which can differentiate between lower and upper case.
+		 // S != s
+		 // Syntax 3: In order to print text we use double quotes.
+		 // Syntax 4: () , {}, “ ” → These should be in pairs.
+		 // Syntax 5: Comments
+		 // Compiler will ignore the statements which are written as comments
+		 // 1.Single Line comment
+		 // // Single Line Comment
+		 // 2.Multi Line comment
+		 // /*This is a multi - line comment*/
+		 // Syntax 6: print() vs println()
+		 // > System.out.print → Prints the output and cursor remains in same line.
+		 // > System.out.println → Prints the output and cursor goes to the next line.
+		 
+
+
+
 	}
 
 }
